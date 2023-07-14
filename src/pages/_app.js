987adab1760e50d56493a5c36a78dd5903f84370
Niels <niels.vder.vlist@gmail.com>
2023-07-14 15:20:05 +0200

    Add changes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,8 @@
 import 'tailwindcss/tailwind.css'
 import '../styles/main.scss'
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
 
 const App = ({ Component, pageProps }) => <Component {...pageProps} />
 
